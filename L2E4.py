cb60f5e87cf9adb03d1ad334f96896d3a62c23e7
isaacmacario <37606217+isaacmacario@users.noreply.github.com>
2024-04-28 23:22:51 -0300

    Update L2E4.py

--- L2E4.py
+++ L2E4.py
@@ -1,21 +1,5 @@
 import numpy as np
 
-def norma_2(n, vetor):
-    
-    # Calculando o módulo dos elementos
-    vetor_modulo = np.abs(vetor)
-    
-    # Elevando ao quadrado
-    vetor_quadrado = vetor_modulo ** 2
-    
-    # Somatório dos elementos
-    somatorio = np.sum(vetor_quadrado)
-    
-    # Raíz quadrada do somatório
-    norma2 = np.sqrt(somatorio)
-    
-    return norma2
-
 def main():
  
     # Definindo valores de n
@@ -27,24 +11,24 @@ def main():
         u = np.random.rand(n, 1)
         v = np.random.rand(n, 1)
 
-        print(f"n = {n}:")
-        print(f"u = {u}")
-        print(f"v = {v}")
+        #print(f"n = {n}:")
+        #print(f"u = {u}")
+        #print(f"v = {v}")
         
         # ||u||_2
-        norma_u = norma_2(n,u)
+        norma_u = np.linalg.norm(u, 2)
         print(f"||u||_2 = {norma_u}")
         
         # ||v||_2
-        norma_v = norma_2(n,v)
+        norma_v = np.linalg.norm(v, 2)
         print(f"||v||_2 = {norma_v}")
         
         A = np.outer(u, v.T)
-        print(f"A = {A}")
-        
+                
         # ||A||_2
-        norma_A = norma_2(n,A)
+        norma_A = np.linalg.norm(A, 2)
         print(f"||A||_2 = {norma_A}")
+        print(f'||u||*||v.T|| = {norma_u*norma_v}')
         
         # Posto de A
         posto_A = np.linalg.matrix_rank(A)
@@ -54,3 +38,4 @@ def main():
     
 if __name__ == "__main__":
     main()
+    
